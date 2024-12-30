0a217d1327baad7b7b0244dc1cb6f4364ef0d38a
jimin <jiminseo0307@gmail.com>
2024-12-31 01:01:45 +0900

    feat: pomodoro app - add reset

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,6 +48,14 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onResetPressed() {
+    setState(() {
+      totalSeconds = setTime;
+      isRunning = false;
+    });
+    timer.cancel();
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
     return duration.toString().substring(2, 7);
@@ -77,13 +85,27 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 3,
             child: Center(
-              child: IconButton(
-                iconSize: 88,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(isRunning
-                    ? Icons.pause_circle_outline
-                    : Icons.play_circle_outlined),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 88,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(isRunning
+                        ? Icons.pause_circle_outline
+                        : Icons.play_circle_outlined),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  IconButton(
+                    iconSize: 32,
+                    color: Theme.of(context).textTheme.displayLarge!.color,
+                    onPressed: isRunning ? onResetPressed : null,
+                    icon: Icon(Icons.refresh_sharp),
+                  ),
+                ],
               ),
             ),
           ),
